16f66d701bf48153de39aca5305b5e4c90daa9a3
Stephen Tongelidis <30709308+stongelidis@users.noreply.github.com>
2022-12-03 21:31:34 -0500

    adds logging and watchdog on source folder

--- ocr_pdf.py
+++ ocr_pdf.py
@@ -1,52 +1,25 @@
 import os
-from time import sleep
 import argparse
+import logging
 
-def find_pfd_files(file_path):
-
-    path_list = []
-
-    contents = os.listdir(file_path)
-
-    for file_name in contents: 
-
-        full_path = os.path.join(file_path, file_name)
-
-        is_file = os.path.isfile(full_path)
-        is_pdf = full_path.endswith('.pdf')
-
-        if is_file and is_pdf:
-            path_list.append(full_path)
-
-    return path_list
+from time import sleep
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
-
-if __name__ == '__main__':
-
-    parser = argparse.ArgumentParser(description = 'Scans a PDF using OCR and saves off a copy')
-    parser.add_argument('source', help='directory path or file path')
-    parser.add_argument('destination', help='destination to save output')
-    parser.add_argument('-d', '--delete', action='store_true', help='delete original file after scan')
-
-    # get arguments
-    args = parser.parse_args()
-
-    # check the source is a file or a directory
-    if os.path.isdir(args.source):
-        files = find_pfd_files(args.source)
-    else:
-        files = [args.source]
+def process_pdfs(list_of_files):
 
     # for each file (file or directory) scan and save to destination
-    for f in files:
-       
+    for f in list_of_files:
+
         # get original source file name and force lower case in destination
         dest_filename = os.path.basename(f)
         dest_filename = dest_filename.lower()
-        
+
         # form destination path
         destination_path = os.path.join(args.destination, dest_filename)
+
+        logging.info(f"Starting OCR on {os.path.basename(f)}")
 
         # command to ocr files
         cmd = f'ocrmypdf --deskew "{f}" "{destination_path}"'
@@ -57,4 +30,82 @@ if __name__ == '__main__':
         if args.delete:
 
             if os.path.exists(destination_path):
-                os.remove(f.replace('"', ''))
+                logging.info(
+                    f"{os.path.basename(f)} found in processing directory {os.path.dirname(destination_path)}"
+                )
+                logging.warning(f"Deleting original file {os.path.basename(f)}")
+                os.remove(f.replace('"', ""))
+
+
+def find_pfd_files(file_path):
+
+    path_list = []
+
+    contents = os.listdir(file_path)
+
+    for file_name in contents:
+
+        full_path = os.path.join(file_path, file_name)
+
+        is_file = os.path.isfile(full_path)
+        is_pdf = full_path.endswith(".pdf")
+
+        if is_file and is_pdf:
+            logging.info(f"Found file {os.path.basename(full_path)}")
+            path_list.append(full_path)
+
+    return path_list
+
+
+class MonitorFolder(FileSystemEventHandler):
+    def on_created(self, event):
+        print(event.src_path, event.event_type)
+        files = find_pfd_files(args.source)
+        process_pdfs(files)
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Scans a PDF using OCR and saves off a copy"
+    )
+    parser.add_argument("source", help="directory path or file path")
+    parser.add_argument("destination", help="destination to save output")
+    parser.add_argument(
+        "-d", "--delete", action="store_true", help="delete original file after scan"
+    )
+
+    # get arguments
+    args = parser.parse_args()
+
+    logging.info("Processing arguments")
+
+    if not os.path.isdir(args.source):
+        assert False, "Error: source needs to be a directory path"
+
+    if not os.path.isdir(args.destination):
+        assert False, "Error: destination needs to be a directory path"
+
+    logging.info(f"Starting Watchdog service on {args.source}")
+
+    observer = Observer()
+    event_handler = MonitorFolder()
+    observer.schedule(event_handler, args.source, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            pass
+
+    except KeyboardInterrupt:
+        logging.warning("Watchdog service interrupted by keyboard input")
+
+    finally:
+        observer.stop()
+        observer.join()
